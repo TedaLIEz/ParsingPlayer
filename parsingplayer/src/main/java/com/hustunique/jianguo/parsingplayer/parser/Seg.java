e19a3661276c43f54897cc35920cc4676cbe5455
CoXier <justcoxier@gmail.com>
2017-01-16 22:08:54 +0800

    add Video Bean

--- parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/Seg.java
+++ parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/Seg.java
@@ -2,7 +2,31 @@ package com.hustunique.jianguo.parsingplayer.parser;
 
 /**
  * Created by CoXier on 17-1-16.
+ * This class represents one part of one video stream.
  */
 
 public class Seg {
+    String path;
+    long duration;
+
+    public Seg(String path, long duration) {
+        this.path = path;
+        this.duration = duration;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
--- parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/VideoInfo.java
+++ parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/VideoInfo.java
@@ -1,10 +1,29 @@
 
 package com.hustunique.jianguo.parsingplayer.parser;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by JianGuo on 1/16/17.
  * POJO for video information extracted from websites
  */
 // TODO: 1/16/17 define fields for information in videos
 public class VideoInfo {
+
+    // now four hds are enough
+    public static int HD_0 = 0;
+    public static int HD_1 = 1;
+    public static int HD_2 = 2;
+    public static int HD_3 = 3;
+
+    private Map<Integer,List<Seg>> segsMap;
+
+    public List<Seg> getSegs(int hd){
+        if (!segsMap.containsKey(hd)) throw new RuntimeException("No such hd in this url");
+        return segsMap.get(hd);
+    }
+    //TODO:construct this class and use HashMap or ?
+
 }
+

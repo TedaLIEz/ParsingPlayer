7a3c585b63abdbd052c3ce420853a90f0ddc71be
TedaLIEz <aliezted@gmail.com>
2017-01-17 12:16:22 +0800

    fix: fix module dependency

--- sample/src/main/java/com/hustunique/jianguo/sample/MainActivity.java
+++ sample/src/main/java/com/hustunique/jianguo/sample/MainActivity.java
@@ -2,8 +2,8 @@
 
 package com.hustunique.jianguo.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

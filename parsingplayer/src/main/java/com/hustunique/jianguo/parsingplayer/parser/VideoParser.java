cf2ca5acfaedc975fcd97100a8a75e9df9ebf748
CoXier <justcoxier@gmail.com>
2017-01-17 11:17:28 +0800

    use regex to check if we support the given url and use Class to load object(may be it has bad performance)

--- parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/VideoParser.java
+++ parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/VideoParser.java
@@ -2,11 +2,35 @@
 
 package com.hustunique.jianguo.parsingplayer.parser;
 
+import android.support.annotation.NonNull;
+
+import com.hustunique.jianguo.parsingplayer.parser.extractor.IExtractor;
+import com.hustunique.jianguo.parsingplayer.parser.extractor.Youku;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by JianGuo on 1/16/17.
  * Parser extracting video info from a given string.
  */
 
 public class VideoParser {
+    private String[] urlRegexArray = {Youku.VALID_URL};
+    private Class[] extratorArray = {Youku.class};
+    private IExtractor iExtractor;
 
+    IExtractor createExtractor(String url) throws IllegalAccessException, InstantiationException {
+        Pattern pattern;
+        Matcher matcher;
+        for (int i=0;i<urlRegexArray.length;i++){
+            pattern = Pattern.compile(urlRegexArray[i]);
+            matcher = pattern.matcher(url);
+            if (matcher.find()){
+                iExtractor = (IExtractor) extratorArray[i].newInstance();
+                return iExtractor;
+            }
+        }
+        throw new RuntimeException("This url is not valid");
+    }
 }
--- parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/extractor/IExtractor.java
+++ parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/parser/extractor/IExtractor.java
@@ -5,7 +5,7 @@ package com.hustunique.jianguo.parsingplayer.parser.extractor;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.hustunique.jianguo.parsingplayer.parser.VideoInfo;
+import com.hustunique.jianguo.parsingplayer.parser.entity.VideoInfo;
 
 /**
  * Created by JianGuo on 1/16/17.
@@ -13,12 +13,6 @@ import com.hustunique.jianguo.parsingplayer.parser.VideoInfo;
  */
 
 public interface IExtractor {
-    /**
-     * return <tt>true</tt> if the host matches
-     * @param host the host of url.
-     * @return <tt>true</tt> if the host matches
-     */
-    boolean hostMatches(@NonNull String host);
     @Nullable
     VideoInfo extract(@NonNull String url);
 }

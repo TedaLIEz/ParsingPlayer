043c3deb91e061de579852f9c08f30951e2ef041
TedaLIEz <aliezted@gmail.com>
2017-01-23 19:37:38 +0800

    feat: mediacontroller & code in sample

---
+++ parsingplayer/src/main/java/com/hustunique/jianguo/parsingplayer/player/ConcatFileTask.java
@@ -0,0 +1,12 @@
+package com.hustunique.jianguo.parsingplayer.player;
+
+/**
+ * Created by JianGuo on 1/23/17.
+ * Utility for ffconcat protocol, so far the way we play multiple videos is to write
+ * a temporary file and set a file uri path to our {@link ParsingVideoView}.
+ * This class is utilized in writing information to file
+ */
+// TODO: 1/23/17 DiskLruCache may be needed here
+public class ConcatFileTask {
+
+}

94b4e34d7b422361ad8995ac53f67f5fed3b83ad
CoXier <justcoxier@gmail.com>
2017-02-22 17:05:38 +0800

    use Class.forName to load class

--- parsingplayer/src/main/java/com/hustunique/parsingplayer/parser/VideoParser.java
+++ parsingplayer/src/main/java/com/hustunique/parsingplayer/parser/VideoParser.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.hustunique.parsingplayer.parser.extractor.QQExtractor;
 import com.hustunique.parsingplayer.util.LogUtil;
 import com.hustunique.parsingplayer.parser.entity.VideoInfo;
 import com.hustunique.parsingplayer.parser.extractor.BilibiliExtractor;
@@ -41,7 +42,7 @@ import java.util.regex.Pattern;
 public class VideoParser {
     private static final String TAG = "VideoParser";
     private Extractor mExtractor;
-    private static Map<String, Class<? extends Extractor>> sMatchMap = new HashMap<>();
+    private static Map<String, String> sMatchMap = new HashMap<>();
 
     private static VideoParser mParser;
 
@@ -56,10 +57,10 @@ public class VideoParser {
     }
 
     static {
-        // TODO: 1/17/17 Maybe there is a better solution to register map between regex and IExtractor here
-        sMatchMap.put(YoukuExtractor.VALID_URL, YoukuExtractor.class);
-        sMatchMap.put(SoHuExtractor.VALID_URL,SoHuExtractor.class);
-        sMatchMap.put(BilibiliExtractor.VALID_URL,BilibiliExtractor.class);
+        sMatchMap.put(YoukuExtractor.VALID_URL, "com.hustunique.parsingplayer.parser.extractor.YoukuExtractor");
+        sMatchMap.put(SoHuExtractor.VALID_URL,"com.hustunique.parsingplayer.parser.extractor.SoHuExtractor");
+        sMatchMap.put(BilibiliExtractor.VALID_URL,"com.hustunique.parsingplayer.parser.extractor.BilibiliExtractor");
+        sMatchMap.put(QQExtractor.VALID_URL,"com.hustunique.parsingplayer.parser.extractor.QQExtractor");
     }
 
     @NonNull
@@ -82,7 +83,11 @@ public class VideoParser {
             Pattern pattern = Pattern.compile(reg);
             Matcher matcher = pattern.matcher(url);
             if (matcher.find()) {
-                return sMatchMap.get(reg);
+                try {
+                    return (Class<? extends Extractor>) Class.forName(sMatchMap.get(reg));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return null;

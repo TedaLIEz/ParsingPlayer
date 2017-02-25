72af7a395a73f996e244a6f94ffca17094916548
CoXier <justcoxier@gmail.com>
2017-02-25 21:04:24 +0800

    setup ci

--- run_tests.sh
+++ run_tests.sh
@@ -18,7 +18,7 @@ if [ ${TEST} == "android" ]; then
     adb shell settings put global window_animation_scale 0.0 
     adb shell settings put global transition_animation_scale 0.0
     adb shell settings put global animator_duration_scale 0.0
-    ./gradlew connectedAndroidTest -PdisablePreDex --stacktrace -i
+    ./gradlew build
 elif [${TEST} == "unit"]; then
     ./gradlew --stacktrace test
 fi
